9c47bea58cacc746bae3f20ae47b11469126ff43
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-07-12 16:53:50 +0900

    [ARM64][Clang] No arm_sve.h / No Clobbering x0-x15

--- kernels/armsve/1m/bli_dpackm_armsve256_asm_8xk.c
+++ kernels/armsve/1m/bli_dpackm_armsve256_asm_8xk.c
@@ -35,6 +35,9 @@
 
 #include "blis.h"
 
+// Clang-vendored arm_sve.h seems conflicting with blis.h.
+#if !defined(__clang__)
+
 #ifdef __ARM_FEATURE_SVE
 #include <arm_sve.h>
 #else
@@ -233,3 +236,6 @@ void bli_dpackm_armsve256_asm_8xk
         );
     }
 }
+
+#endif
+
