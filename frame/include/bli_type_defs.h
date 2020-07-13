20737498fdfdd8bd04c22570e019d56ec780512e
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-07-14 04:51:30 +0900

    Added Sub-configuration Set for Arm SVE
    - This subconfig is created following `docs/ConfigurationHowTo.md`;
    - GEMM kernels are selected at run time according to effect of the
      `incd` instruction;
    - This subconfig is currently named armsve.

--- frame/include/bli_type_defs.h
+++ frame/include/bli_type_defs.h
@@ -1005,6 +1005,7 @@ typedef enum
 	BLIS_ARCH_THUNDERX2,
 	BLIS_ARCH_CORTEXA57,
 	BLIS_ARCH_CORTEXA53,
+	BLIS_ARCH_ARMSVE,
 	BLIS_ARCH_CORTEXA15,
 	BLIS_ARCH_CORTEXA9,
 
@@ -1021,7 +1022,7 @@ typedef enum
 
 // NOTE: This value must be updated to reflect the number of enum values
 // listed above for arch_t!
-#define BLIS_NUM_ARCHS 22
+#define BLIS_NUM_ARCHS 23
 
 
 //
