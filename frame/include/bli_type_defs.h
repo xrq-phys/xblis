d3b2bc0fd00701f8c22b2ceaa2147a59a61ca43b
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-07-19 06:49:01 +0900

    Added Sub-configuration Set for Fujitsu A64fx
    - This subconfig is created following `docs/ConfigurationHowTo.md`;
    - 512-bit SVE and 512-bit cache block size is assumed for performance;
    - GEMM kernel fixed to A64fx kernels as indexed FMLA is very slow on
      the A64fx chip;

--- frame/include/bli_type_defs.h
+++ frame/include/bli_type_defs.h
@@ -1004,6 +1004,7 @@ typedef enum
 	BLIS_ARCH_CORTEXA57,
 	BLIS_ARCH_CORTEXA53,
 	BLIS_ARCH_ARMSVE,
+	BLIS_ARCH_A64FX,
 	BLIS_ARCH_CORTEXA15,
 	BLIS_ARCH_CORTEXA9,
 
@@ -1019,7 +1020,7 @@ typedef enum
 
 // NOTE: This value must be updated to reflect the number of enum values
 // listed above for arch_t!
-#define BLIS_NUM_ARCHS 22
+#define BLIS_NUM_ARCHS 23
 
 
 //
